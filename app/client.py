d57cfff524408ddd821063e9b490199142655cb4
SmartSpace <jaysys@gmail.com>
2025-08-21 10:19:07 +0900

    Update client.py
    안내문구 개선

--- app/client.py
+++ app/client.py
@@ -14,13 +14,16 @@ def try_mcp_server(query: str):
 def smart_query(query: str):
     mcp_result = try_mcp_server(query)
     if mcp_result is not None:
-        return f"[MCP 서버 응답]\n{mcp_result}"
+        return f" -> MCP 서버가 응답합니다\n{mcp_result}"
     else:
-        print(" -> 질문내용운 제공하는 MCP서버와 관련 없음. LLM으로 답변하겠습니다.")
-    return f"[LLM 직접 응답]\n{ask_llm_general_question(query)}"
+        print(" -> 질의하신 내용은 MCP기능과 관련이 없는것 같습니다. 오픈소스 LLM이 답변드리겠습니다.")
+    return f"{ask_llm_general_question(query)}"
 
 if __name__ == "__main__":
-    questions = ["9단 알려줘", "대한민국의 인구는 얼마야?","80단 알려줘"]
+    questions = ["9단 알려줘", 
+                "대한민국의 인구는 얼마야?",
+                "80단 알려줘봐"]
     for q in questions:
-        print(f"\n질문: {q}")
+        print(f"\nQuestion💬 '{q}'")
         print(smart_query(q))
+        
